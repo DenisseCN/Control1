86edd2b3735aaa15f0ff11a20cdc2078372d1999
DenisseCoilla <de.coilla@duocuc.cl>
2024-09-11 20:11:54 -0300

    Ingresar,Correo,Pregunta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,34 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+// ESTO ES NECESARIO PARA LAS ANIMACIONESPARA USAR ANGULAR MATERIAL
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { LOCALE_ID } from '@angular/core';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+      BrowserModule
+    , IonicModule.forRoot({ innerHTMLTemplatesEnabled: true })
+    , AppRoutingModule
+    , BrowserAnimationsModule
+    , MatDatepickerModule
+  ],
+  providers: [
+    { 
+      provide: RouteReuseStrategy, 
+      useClass: IonicRouteStrategy 
+    }
+    , provideAnimationsAsync()
+    , {
+      provide: LOCALE_ID,
+      useValue: 'es-CL'
+    }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
